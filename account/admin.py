e359889226d691bbfde04335d4a166ca6ec59ba9
Navanjane <nkindelpitiya@gmail.com>
2021-06-10 12:40:07 +0530

    CSV export added to the admin

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,10 @@
+import unicodecsv as unicodecsv
 from django.contrib import admin
 from .models import User, TeacherProfile, StudentProfile, GroupAdminForm, Group, StaffProxyModel
 from django.contrib.auth.admin import UserAdmin
 from rest_framework.authtoken.models import Token
+import csv
+from django.http import HttpResponse
 
 #Register your models here.
 
@@ -24,13 +27,64 @@ class StudentAdmin(admin.ModelAdmin):
     model = StudentProfile
     search_fields = ['user__email','user__username']
 
+    def export_as_csv(self, request, queryset):
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+
+    actions = ["export_as_csv"]
+
 
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
 
 class StaffProxyModelAdmin(UserAdmin):
+
+    def export_as_csv_action(description="Export selected objects as CSV file",
+                             fields=None, exclude=None, header=True):
+
+
+        def export_as_csv(modeladmin, request, queryset):
+            opts = modeladmin.model._meta
+
+            if not fields:
+                field_names = [field.name for field in opts.fields]
+            else:
+                field_names = fields
+
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts)
+
+            writer = unicodecsv.writer(response, encoding='utf-8')
+            if header:
+                writer.writerow(field_names)
+            for obj in queryset:
+                row = [getattr(obj, field)() if callable(getattr(obj, field)) else getattr(obj, field) for field in
+                       field_names]
+                writer.writerow(row)
+            return response
+
+        export_as_csv.short_description = description
+        return export_as_csv
+
+
     search_fields = ['email', 'username']
+    list_display = ('email', 'username', 'telegram_number', 'district', 'phone_no', 'parent_number')
+    #actions = ["export_as_csv"]
+    actions = [export_as_csv_action("CSV Export", fields=['email', 'username', 'telegram_number', 'district', 'phone_no', 'parent_number'])]
+
+
 
     filter_horizontal = ()
     list_filter = ()
